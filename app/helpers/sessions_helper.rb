f19453071b7ac87fd653461b3de533afb62e8bb5
Fiona Tran <waterfallofdestiny@gmail.com>
2011-05-11 13:51:08 -0400

    Done with user edit/update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,9 +6,18 @@ module SessionsHelper
       self.current_user = user
   end
   
+  def current_user=(user)
+    @current_user = user
+  end 
+  
   def current_user
     @current_user ||= user_from_remember_token
   end 
+  
+  def current_user?(user)
+    user == current_user
+  end 
+  
   
   def user_from_remember_token
     remember_token = cookies[:remember_token]
@@ -22,5 +31,24 @@ module SessionsHelper
   def sign_out
     cookies.delete(:remember_token) 
     self.current_user = nil
+  end 
+  
+  def deny_access
+    store_location
+    flash[:notice] = "Please sign in to access this page." 
+    redirect_to signin_path
+  end 
+  
+  def store_location
+    session[:return_to] = request.request_uri
+  end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
+  end 
+  
+  def clear_return_to
+    session[:return_to] = nil
   end 
 end
